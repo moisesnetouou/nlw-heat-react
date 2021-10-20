8f20c09b2b91c4aa417a783f57a09d4b37810bc7
Moisés Neto <moisesnetored@gmail.com>
2021-10-20 15:35:19 -0400

    feature: Componente de enviar mensagens funcional

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useContext } from "react";
 import styles from "./App.module.scss";
 import { LoginBox } from "./components/LoginBox";
+import { SendMessageForm } from "./components/SendMessageForm";
 import { MessageList } from "./components/MessageList";
+import { AuthContext } from "./context/auth";
 
 export function App() {
+  const { user } = useContext(AuthContext);
+
   return (
-    <main className={styles.contentWrapper}>
+    <main
+      className={`${styles.contentWrapper} ${
+        !!user ? styles.contentSigned : ""
+      }`}
+    >
       <MessageList />
-      <LoginBox />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
   );
 }
